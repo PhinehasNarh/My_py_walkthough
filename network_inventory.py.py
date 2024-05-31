1b32dfc3ee883c4ec9e4215144b2d1bc8b219d6f
Narh Phinehas <phinehastettehnarh@gmail.com>
2024-05-31 04:25:53 -0700

    Add files via upload
    This Python script is like a handy notebook for keeping track of my network devices. I can start with a list of devices I have, and then add new ones as I find them. It also helps me sort them alphabetically, so they're easier to find later. I can even use it to see how many of a certain device I have, or where a specific device is on the list. On top of that, it lets me look at specific devices based on their position on the list, or grab a range of them at once. If I need to add a new device in a particular spot, I can do that too. And if things get messy, I can just clear the whole list and start fresh. There's even a bit of extra code at the end that shows how to loop through a list to print things, like all the even numbers from 0 to 8.

---
+++ network_inventory.py.py
@@ -0,0 +1,49 @@
+network_devices=['cisco1','fortinet2','aS4D7','linksys8','nETGEAR04','nETGEAR05']
+
+
+#extending(adds the string elemnts)
+new_devices=['router5','moderm2']
+network_devices.extend(new_devices)
+print(network_devices)
+
+#sorting(sorts according in ascending order)
+network_devices.sort()
+print(network_devices)
+
+#counting(counts how many times each string element appeared )
+print(network_devices.count('nETGEAR05'))
+
+#indexing(shows the number position according to how its listed/sorted)
+network_item=network_devices.index('fortinet2')
+print(network_item)
+#or 
+#can use this instead, looks pretty easy to me,prefer this one more 
+print(network_devices[7])
+
+#range(diplays the range of lements within a list)
+print(network_devices[1:6])
+
+
+#inserting a string into a list and specifying its position
+print(network_devices.insert(4,'linksys12'))
+print(network_devices)
+
+
+#reversing the whole list backwards
+network_devices.reverse()
+print(network_devices)
+
+#clear the whole elements in the list to an empty list
+network_devices.clear()
+print(network_devices)
+
+
+
+storage_devices=['pendrive','harddisk']
+network_devices.extend(storage_devices)
+print(network_devices)
+
+
+x=range(0,10,2)
+for n in x:
+    print(n)
