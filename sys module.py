93cb2a63891adcc09ab1d35e851c6f558ea52b4b
Narh Phinehas <phinehastettehnarh@gmail.com>
2024-08-05 15:56:25 +0100

    Add files via upload

---
+++ sys module.py
@@ -0,0 +1,7 @@
+import sys
+print("Platform:",sys.platform)
+print(sys.stdout.write("writiing in standard output"))
+print("Python Version:", sys.version)
+print("Default encoding:", sys.getfilesystemencoding())
+print("System encoding:", sys.getdefaultencoding())
+print("Path environment variable:", sys.path)
