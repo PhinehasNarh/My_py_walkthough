2007e77f7d724fa530d83cd21d76371dec8ce23e
ph1n3y <phinehastettehnarh@gmail.com>
2024-11-22 15:54:59 +0000

    Create type_error_checking_conversion.py

---
+++ type_error_checking_conversion.py
@@ -0,0 +1,14 @@
+
+#Wrote out 4 type checks to print all 4 data types
+print(type((12345)))
+print(type((False)))
+print(type((3.14159)))
+
+
+
+#task
+#print("Number of letters in your name: " , len(input("Enter your name"))))
+#couldn't run because the syntax dosen't allow str to int concatination.
+
+#results
+print("Number of letters in your name: " +  str(len(input("Enter your name"))))
