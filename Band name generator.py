5c7589c870e47498ee2264e696e9d492d7ff6bc2
ph1n3y <phinehastettehnarh@gmail.com>
2024-11-26 23:21:11 +0000

    Update Band name generator.py

--- Band name generator.py
+++ Band name generator.py
@@ -2,3 +2,5 @@ print("Welcome to the Band Name Generator!")
 city= input("What is the name of the city you grew up in?\n")
 pet= input("what is the name of your pet?\n")
 print("Your Band name could be " + city + " " + pet)
+
+#ph1n3y
