56a13a493898d3353065fce4f87c855a03347c15
ph1n3y <phinehastettehnarh@gmail.com>
2024-11-26 23:21:32 +0000

    Update Tip_calculator.py

--- Tip_calculator.py
+++ Tip_calculator.py
@@ -4,3 +4,5 @@ tip = float(input("What percentage tip would you like to give? "))
 split= int(input("How many people to split the bill? "))
 total_bill = round((bill + (bill * (tip / 100)))/split, 2)
 print(f"Each person should pay: ${total_bill}, not even a dime less")  
+
+#ph1n3y
