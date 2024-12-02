200a36eb7b27def7b8b90c60ca7ad1d68306ede3
ph1n3y <phinehastettehnarh@gmail.com>
2024-12-02 13:52:59 +0000

    Update Randomization.py

--- Randomization.py
+++ Randomization.py
@@ -2,3 +2,8 @@
 import random
 random_number= random.randint(1,10)
 print(random_number)
+
+# prints a random float between 0.0 and < 1.0
+import random
+random_number_0_to_1 =random.random()
+print(random_number_0_to_1)  
