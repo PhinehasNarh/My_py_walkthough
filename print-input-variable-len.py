fbf2ead8967fc2a6edee61d7406e5c03f14c85af
ph1n3y <phinehastettehnarh@gmail.com>
2024-12-02 14:05:54 +0000

    Update print-input-variable-len.py

--- print-input-variable-len.py
+++ print-input-variable-len.py
@@ -1,3 +1,4 @@
+#Ask to input name and prints out the number of characters
 name = input("What is your name?")
 name_len = len(name)
 print("My name is " + name + " and it's " + str(name_len) + " characters long!")
