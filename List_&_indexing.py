dcb412f386ed669ce491e6a68ad00289482ae085
ph1n3y <phinehastettehnarh@gmail.com>
2024-12-09 14:55:22 +0000

    Create List_&_indexing.py

---
+++ List_&_indexing.py
@@ -0,0 +1,19 @@
+#LIST AND INDEXING
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+# indexing to print each item
+print(states_of_america[12])
+# to add an item to a list
+states_of_america.append("ph1n3yland")
+# to add a list of items to the existing list
+states_of_america.extend(["Westwood", "Windsor", "Worcester"])
+print(states_of_america)
+
+#NESTED LISTS
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears",
+# "Tomatoes", "Celery", "Potatoes"]
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
