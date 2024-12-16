ded83ff71ccf50152bb37a89cd02e8735cb77afb
ph1n3y <phinehastettehnarh@gmail.com>
2024-12-16 15:26:52 +0000

    Update loops.py

--- loops.py
+++ loops.py
@@ -3,3 +3,12 @@ for fruit in fruits:
     print(fruit)
     print(fruit + " fruit")
 print(fruits)
+
+
+#using for loops to write the max function i.e its the function that outputs the highest value of a list of items
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+max_score=0
+for score in student_scores:
+    if score > max_score:
+        max_score=score
+print(max_score)
