486b28b06e1aa1b2dcdba1b07846d9a67515f3ed
ph1n3y <phinehastettehnarh@gmail.com>
2025-03-27 16:31:46 +0000

    Update Reeborg's world(Hurdles challenge).py

--- Reeborg's world(Hurdles challenge).py
+++ Reeborg's world(Hurdles challenge).py
@@ -1,4 +1,4 @@
-#Reeborg's world(Hurdles challenge)
+#Reeborg's world(Hurdles challenge 1)
 
 def turn_right():
     turn_left()
@@ -24,5 +24,26 @@ number_of_hurdles=6
 while number_of_hurdles>0:
     jump()
     print(number_of_hurdles)
+
+
+#Reeborg's world(Hurdles challenge 2)
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def over_wall():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+ 
+
+while not at_goal() !=False:
+    over_wall()
 
 #ph1n3y
